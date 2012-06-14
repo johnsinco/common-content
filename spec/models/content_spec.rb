cb1e801697d50b3fd3a35409b14de865a59de40b
John Stewart <lead.j2ee.engineer@gmail.com>
2012-06-13 21:42:17 -0600

    more specs for content

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -5,4 +5,21 @@ describe Content do
     Content.new(slug:'foo', defn:'# none').should be_valid
     Content.new.should_not be_valid
   end
+  it 'provides direct access to the content definition' do
+    c = Content.new(slug:'foo', defn:'# none')
+    c.definition.definition.should == '# none'
+  end
+  context '#build' do
+    it 'builds a unique instance for each call' do
+      a = Content.build('aaa', 'field :a, type: String', binding)
+      a.class.name.should == 'Aaa'
+      a.should respond_to(:a)
+      a.should_not respond_to(:b)
+      a.fields.keys.should_not include(:b)
+      b = Content.build('bbb', 'field :b, type: String', binding)
+      b.class.name.should == 'Bbb'
+      b.should respond_to(:b)
+      b.should_not respond_to(:a)
+    end
+  end
 end
