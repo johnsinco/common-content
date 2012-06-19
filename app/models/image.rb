00b8e90d5dc5167d8649e7197c19d2921ac3c0cd
John Stewart <lead.j2ee.engineer@gmail.com>
2012-06-18 20:47:06 -0600

    image type

---
+++ app/models/image.rb
@@ -0,0 +1,11 @@
+class Image < String
+  include Mongoid::Fields::Serializable
+
+  def serialize(object) 
+    object.to_s
+  end
+  def deserialize(object)
+    Image.new(object)
+  end 
+
+end
---
+++ spec/models/image_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Image do
+  let(:content) { Content.build('image-holder', 'field :img, type: Image')}
+  it 'can save an image url' do
+    Image.new('foo.png').should == 'foo.png'
+  end
+  it 'can be included in a Content item' do
+    content.img = 'foo.png'
+    content.save
+    c = Content.find(content.id)
+    c.img.class.to_s.should == 'Image'
+    c.img.should == 'foo.png'
+  end
+end
