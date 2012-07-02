7be7f78d23d586637b9db328afc9a41b9249d39b
John Stewart <lead.j2ee.engineer@gmail.com>
2012-07-02 11:10:15 -0600

    switched contents.js to coffeescript to follow railscast that was copied from. updated helpers

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -19,11 +19,9 @@ module ContentsHelper
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
-p "fields = f.fields_for(#{association}, #{new_object}, child_index: #{id}) do |builder|"
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(partial: association.to_s.singularize + "_fields", locals: { f: builder })
     end
-p "link_to_add_fields #{name}, #{f} #{association} fields=#{fields}"
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
